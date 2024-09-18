f8bec378f219b8792dedf0069075f4b089d40fd0
Alex1um <ye4h@pm.me>
2024-09-18 13:28:18 +0700

    tests begin

--- src/ObjectOriented/Participants/Participant.cs
+++ src/ObjectOriented/Participants/Participant.cs
@@ -5,11 +5,6 @@ abstract public class Participant()
     public int Id { get; set; }
     public string Name { get; set; }
 
-    // public Participant(int id, string name) : this() {
-    //     Id = id;
-    //     Name = name;
-    // }
-
     public static List<T> createList<T>(List<T> other_list) where T : Participant
     {
         return other_list.GetShuffled();
