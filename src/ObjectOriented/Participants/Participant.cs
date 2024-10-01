b59ffe29b2970ef6103c211dcb84d69680168748
Alex1um <ye4h@pm.me>
2024-10-01 14:53:14 +0700

    Revert "tests begin"
    This reverts commit f8bec378f219b8792dedf0069075f4b089d40fd0.

--- src/ObjectOriented/Participants/Participant.cs
+++ src/ObjectOriented/Participants/Participant.cs
@@ -5,6 +5,11 @@ abstract public class Participant()
     public int Id { get; set; }
     public string Name { get; set; }
 
+    // public Participant(int id, string name) : this() {
+    //     Id = id;
+    //     Name = name;
+    // }
+
     public static List<T> createList<T>(List<T> other_list) where T : Participant
     {
         return other_list.GetShuffled();
