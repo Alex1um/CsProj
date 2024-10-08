87d632a816a10bccdeba52fd592cf331e83167ce
Alex1um <ye4h@pm.me>
2024-10-09 02:51:30 +0700

    replaced preferencesstore with ReadOnlyDictionary(types are same)

--- HackatonService/src/PreferencesStore.cs
+++ HackatonService/src/PreferencesStore.cs
@@ -1,29 +1,13 @@
 namespace HackatonService;
 
-using System.Collections.Frozen;
-using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
 using HackatonService.Participants;
 
-public class PreferencesStore<T, V> where T : notnull where V : Participant
+public class PreferencesStore<T, V> : ReadOnlyDictionary<T, List<V>> where T : notnull where V : Participant
 {
-    private Dictionary<T, List<V>> _Dict = new();
 
-    public PreferencesStore(List<T> t, List<V> v) : base()
-    {
-        foreach (var item in t)
-        {
-            _Dict.Add(item, Participant.CreateList(v));
-        }
-    }
+    public PreferencesStore(List<T> t, List<V> v) : base(t.ToDictionary(x => x, x => Participant.CreateList(v))) { }
 
-    public List<V> this[T key] { get => _Dict[key]; }
-
-    public Dictionary<T, List<V>>.Enumerator GetEnumerator() {
-        return _Dict.GetEnumerator();
-    }
-
-    public int Count { get => _Dict.Count; }
-
-    public PreferencesStore(Dictionary<T, List<V>> dict) { _Dict = dict; }
+    public PreferencesStore(Dictionary<T, List<V>> dict) : base(dict) { }
 
 }
