0271111e226a357f24d4d9c6197c02cd85215967
Alex1um <ye4h@pm.me>
2024-11-20 03:06:55 +0700

    Revert "added hackaton integration test"
    This reverts commit d8e2fb7f7892edf40aa682ee1760eda13270ccfa.

--- HackatonService/src/Hackaton.cs
+++ HackatonService/src/Hackaton.cs
@@ -5,31 +5,16 @@ using HackatonService.Settings;
 using HackatonService.DataIO;
 using Microsoft.Extensions.Options;
 
-class Hackaton
+class Hackaton(IOptions<DataSourceSettings> sourcesSettings)
 {
-    private readonly List<Teamlead> Teamleads;
-    private readonly List<Junior> Juniors;
-
-    private readonly PreferencesStore<Junior, Teamlead> juniorsTeamleads;
-    private readonly PreferencesStore<Teamlead, Junior> teamleadsJuniors;
-
-    public Hackaton(IOptions<DataSourceSettings> sourcesSettings) {
-        Teamleads = CSVReader.Read<Teamlead>(sourcesSettings.Value.TeamleadsListPath);
-        Juniors = CSVReader.Read<Junior>(sourcesSettings.Value.JuniorsListPath);
-        juniorsTeamleads = new PreferencesStore<Junior, Teamlead>(Juniors, Teamleads);
-        teamleadsJuniors = new PreferencesStore<Teamlead, Junior>(Teamleads, Juniors);
-    }
-
-    public Hackaton(List<Teamlead> teamleads, List<Junior> juniors, PreferencesStore<Junior, Teamlead> juniorsTeamleads, PreferencesStore<Teamlead, Junior> teamleadsJuniors) {
-        Teamleads = teamleads;
-        Juniors = juniors;
-        this.juniorsTeamleads = juniorsTeamleads;
-        this.teamleadsJuniors = teamleadsJuniors;
-    }
+    private readonly List<Teamlead> Teamleads = CSVReader.Read<Teamlead>(sourcesSettings.Value.TeamleadsListPath);
+    private readonly List<Junior> Juniors = CSVReader.Read<Junior>(sourcesSettings.Value.JuniorsListPath);
 
     public double Run(HRManager manager, HRDirector director) {
-        var resultDict = manager.BuildTeams(Teamleads, Juniors, teamleadsJuniors, juniorsTeamleads);
-        return director.CalculateHarmonicMean(teamleadsJuniors, juniorsTeamleads, resultDict);
+        var junLists = new PreferencesStore<Junior, Teamlead>(Juniors, Teamleads);
+        var teamleadLists = new PreferencesStore<Teamlead, Junior>(Teamleads, Juniors);
+        var resultDict = manager.BuildTeams(Teamleads, Juniors, teamleadLists, junLists);
+        return director.CalculateHarmonicMean(teamleadLists, junLists, resultDict);
     }
 
 }
